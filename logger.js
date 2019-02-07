d55d0ccdd2164351d5b96c71bfe274cc47b06a38
Beatriz Tovar <beatrizctovar@gmail.com>
2019-02-06 17:45:08 -0800

    Adding middleware

--- logger.js
+++ logger.js
@@ -1,15 +1,6 @@
-const EventEmitter = require("events"); //class
-
-let url = "http://mylogger.io/log";
-
-class Logger extends EventEmitter {
-  log(message) {
-    //Send an HTTP request
-    console.log(message);
-
-    //Raise an event
-    this.emit("messageLogged", { id: 1, url: "http://" }); //signaling event has happened
-  }
+function log(req, res, next) {
+  console.log("Logging....");
+  next();
 }
 
-module.exports = Logger;
+module.exports = log;
